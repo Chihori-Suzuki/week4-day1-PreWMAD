6d98b2760f6805be3918a6c4eb0b0fc489955149
鈴木ちほり <suzukichihori@suzukichihoris-MacBook-Air.local>
2020-08-24 11:51:46 -0700

    third commit

--- addLargestNumbers.js
+++ addLargestNumbers.js
@@ -2,16 +2,29 @@ const addLargestNumbers = function(data) {
   // Put your solution here
   let answer = 0;
 
-  function compareNumbers(a, b) {
-    return a - b;
-  }
-  data.sort(compareNumbers);
-  answer = data[data.length-1] + data[data.length-2];
+  /* another answer */
+  // function compareNumbers(a, b) {
+  //   return a - b;
+  // }
+  // data.sort(compareNumbers);
+  // answer = data[data.length-1] + data[data.length-2];
 
+  let longest = 0;
+  let secLongest = 0;
+  for (i in data){
+    for(j in data){
+      if (data[j] > longest){
+        longest = data[j];
+      }else if(data[j]< longest && data[j] > secLongest){
+        secLongest = data[j];
+      }
+    }
+  }
+  answer = longest + secLongest;
   return answer;
 };
 
-console.log(addLargestNumbers([1, 10]));
-console.log(addLargestNumbers([1, 2, 3]));
+// console.log(addLargestNumbers([1, 10]));
+// console.log(addLargestNumbers([1, 2, 3]));
 console.log(addLargestNumbers([10, 4, 34, 6, 92, 2]));
 
